130add564a30e8c93851222f167adf6a56357801
Jeton Thaçi <jetonthaci@outlook.com>
2019-03-19 15:10:36 +0100

    define bubble sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,5 +1,14 @@
-def bubble_sort_by(arr)
+def bubble_sort(a)
 
-    arr
+    (a.length-1).times {       
+        for i in 0...a.length-1
+            a[i], a[i+1] = a[i+1], a[i] if a[i] > a[i+1]
+        end
+    }
+    a
 
 end
+
+a = [9,8,7,1,6]
+
+puts bubble_sort(a)
