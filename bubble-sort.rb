aa40e78afa9b52c1cb435ece0475668adeccdcc9
1ba1 <vvvalenti01@yahoo.it>
2019-03-19 15:48:20 +0100

    implemented bubble_sort and bubble_sort_by

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,14 +1,20 @@
 def bubble_sort(a)
-
-    (a.length-1).times {       
-        for i in 0...a.length-1
-            a[i], a[i+1] = a[i+1], a[i] if a[i] > a[i+1]
-        end
-    }
-    a
-
+  (a.length-1).times {
+      for i in 0...a.length-1
+        a[i], a[i+1] = a[i+1], a[i] if a[i] > a[i+1]
+      end
+  }
+  a
 end
 
-a = [9,8,7,1,6]
+def bubble_sort_by(a)
+  (a.length-1).times {
+      for i in 0...a.length-1
+        a[i], a[i+1] = a[i+1], a[i] if yield(a[i], a[i+1]) > 0
+      end
+  }
+  a
+end
 
-puts bubble_sort(a)
+puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
+# ["hi", "hey", "hello"]
