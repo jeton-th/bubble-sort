1cc2d830252fe1e0569c5c74724d57b7dd99605e
Jeton Thaçi <jetonthaci@outlook.com>
2019-03-21 12:39:50 +0100

    fix bugs and linting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,22 @@
-def bubble_sort(a)
-  (a.length - 1).times do
-    for i in 0...a.length - 1
-      a[i], a[i+1] = a[i+1], a[i] if a[i] > a[i+1]
+def bubble_sort(arr)
+  (arr.length - 1).times do
+    for i in 0...arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
     end
+  end
 
-  a
+  arr
 end
 
-def bubble_sort_by(a)
-  (a.length-1).times do
-    for i in 0...a.length - 1
-      a[i], a[i + 1] = a[i + 1], a[i] if yield(a[i], a[i + 1]) > 0
+def bubble_sort_by(arr)
+  (arr.length - 1).times do
+    for i in 0...arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]) > 0
     end
+  end
 
-  a
+  arr
 end
 
-puts bubble_sort_by(["hi", "hello", "hey"]) { |left, right| left.length - right.length }
-
+arr = %w[hi hello hey bye goodbye]
+puts bubble_sort_by(arr) { |left, right| left.length - right.length }
